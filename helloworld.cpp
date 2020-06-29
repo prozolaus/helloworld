3254f59198b37d0e94c647d582193382f7c2a0d3
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-06-29 11:53:27 +0300

    initial project version

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+ /*   vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << "\n";
+    }
+*/
+    int a = 0;
+    int b = 0;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << " The sum is " << a+b << endl;
+}
