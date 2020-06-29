2dc8284ac453f5f05aef569b33e0fc5ce0413222
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-06-29 13:23:06 +0300

    Addition folder .vscode/ and some changes

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,18 +6,14 @@ using namespace std;
 
 int main()
 {
- /*   vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << "\n";
-    }
-*/
     int a = 0;
     int b = 0;
+    int c = 0;
     cout << "Enter a: ";
     cin >> a;
     cout << "Enter b: ";
     cin >> b;
-    cout << " The sum is " << a+b << endl;
+    cout << "Enter c: ";
+    cin >> c;
+    cout << " The sum is " << a+b+c << endl;
 }
