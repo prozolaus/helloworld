21437950e7018d49f1fef6b2c9eb3b11d76ce5a5
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-07-01 00:28:31 +0300

    ex. 3.3

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,14 +6,24 @@ using namespace std;
 
 int main()
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    cout << "Enter a: ";
-    cin >> a;
-    cout << "Enter b: ";
-    cin >> b;
-    cout << "Enter c: ";
-    cin >> c;
-    cout << "The sum of integers (a, b, c) is " << a+b+c << endl;
+    int val1 = 0;
+    int val2 = 0;
+    int min = 0;
+    int max = 0;
+    cout << "Enter first number: ";
+    cin >> val1;
+    cout << "Enter second number: ";
+    cin >> val2;
+    max = (val1 > val2) ? val1 : val2;
+    min = (val1 < val2) ? val1 : val2;
+    cout << "The largest number is " << max << endl;
+    cout << "The smallest number is " << min << endl;
+    cout << "The sum of these numbers is " << val1 + val2 << endl;
+    cout << "The difference of these numbers is " << max - min << endl;
+    cout << "The product of these numbers is " << val1 * val2 << endl;
+    cout << "The division of these numbers is " << (double)max / min << endl;
+
+    
+
+
 }
