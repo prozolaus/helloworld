5cb03881cecc3fea9eba5b126b4f6c11686220ab
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-07-03 00:39:23 +0300

    ex. 3.6

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,24 +6,40 @@ using namespace std;
 
 int main()
 {
-    int val1 = 0;
-    int val2 = 0;
-    int min = 0;
-    int max = 0;
-    cout << "Enter first number: ";
-    cin >> val1;
-    cout << "Enter second number: ";
-    cin >> val2;
-    max = (val1 > val2) ? val1 : val2;
-    min = (val1 < val2) ? val1 : val2;
-    cout << "The largest number is " << max << endl;
-    cout << "The smallest number is " << min << endl;
-    cout << "The sum of these numbers is " << val1 + val2 << endl;
-    cout << "The difference of these numbers is " << max - min << endl;
-    cout << "The product of these numbers is " << val1 * val2 << endl;
-    cout << "The division of these numbers is " << (double)max / min << endl;
-
-    
-
-
+    int a, b, c, min1, min2, min3;
+    cout << "Enter three numbers through space: ";
+    cin >> a >> b >> c;
+    min1 = (a < b) ? a : b;
+    if (min1 > c) min1 = c;
+    if (min1 == a) {
+        if (b < c) {
+            min2 = b;
+            min3 = c;
+        }
+        else {
+            min2 = c;
+            min3 = b;
+        }
+    }
+    else if (min1 == b) {
+        if (a < c) {
+            min2 = a;
+            min3 = c;
+        }
+        else {
+            min2 = c;
+            min3 = a;
+        }
+    }
+    else if (min1 == c) {
+        if (a < b) {
+            min2 = a;
+            min3 = b;
+        }
+        else {
+            min2 = b;
+            min3 = a;
+        }
+    }
+    cout << "Numbers are sorted: " << min1 << ' ' << min2 << ' ' << min3 << endl;
 }
