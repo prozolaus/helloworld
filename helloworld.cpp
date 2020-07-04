7c8aab33b8a83743041795fe22c4fb1c76bc58f4
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-07-04 10:04:10 +0300

    ex. 3.11

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,40 +6,41 @@ using namespace std;
 
 int main()
 {
-    string a, b, c, min1, min2, min3;
-    cout << "Enter three words through space: ";
-    cin >> a >> b >> c;
-    min1 = (a < b) ? a : b;
-    if (min1 > c) min1 = c;
-    if (min1 == a) {
-        if (b < c) {
-            min2 = b;
-            min3 = c;
-        }
-        else {
-            min2 = c;
-            min3 = b;
-        }
+    int cent1 = 0, cent5 = 0, cent10 = 0, cent25 = 0, cent50 = 0, sum = 0;
+    cout << "How many one-cent coins do you have? ";
+    cin >> cent1;
+    cout << "How many five-cent coins do you have? ";
+    cin >> cent5;
+    cout << "How many ten-cent coins do you have? ";
+    cin >> cent10;
+    cout << "How many twenty five-cent coins do you have? ";
+    cin >> cent25;
+    cout << "How many fifty-cent coins do you have? ";
+    cin >> cent50;
+    cout << endl;
+    if (cent1 > 0) {
+        cout << "You have " << cent1 << " one-cent coin";
+        if (cent1 > 1) cout << "s."; else cout << "."; cout << endl;
     }
-    else if (min1 == b) {
-        if (a < c) {
-            min2 = a;
-            min3 = c;
-        }
-        else {
-            min2 = c;
-            min3 = a;
-        }
+    if (cent5 > 0) {
+        cout << "You have " << cent5 << " five-cent coin";
+        if (cent5 > 1) cout << "s."; else cout << "."; cout << endl;
     }
-    else if (min1 == c) {
-        if (a < b) {
-            min2 = a;
-            min3 = b;
-        }
-        else {
-            min2 = b;
-            min3 = a;
-        }
+    if (cent10 > 0) {
+        cout << "You have " << cent10 << " ten-cent coin";
+        if (cent10 > 1) cout << "s."; else cout << "."; cout << endl;   
     }
-    cout << "The words are sorted: " << min1 << ' ' << min2 << ' ' << min3 << endl;
+    if (cent25 > 0) {
+        cout << "You have " << cent25 << " twenty five-cent coin";
+        if (cent25 > 1) cout << "s."; else cout << "."; cout << endl;
+    }
+    if (cent50 > 0) {
+        cout << "You have " << cent50 << " fifty-cent coin";
+        if (cent50 > 1) cout << "s."; else cout << "."; cout << endl;
+    }
+    sum = cent1 + cent5 * 5 + cent10 * 10 + cent25 * 25 + cent50 * 50;
+    cout << "\nTotal value of your coins is " << sum/100 << " dollar";
+    if ((sum/100)!=1) cout << "s "; else cout << " ";
+    cout << sum%100 << " cents.\n\n";
+
 }
