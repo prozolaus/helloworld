d9960d9ec24674641cea7c632a775afef1a74647
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-09-05 22:30:20 +0300

    ex. 4.5 (calculator)

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,33 +7,33 @@ using namespace std;
 
 int main()
 {
-   int x = 51, m = 1, n = 101, i = 1, count = 0;
-    bool flag = false;
-    double d = 0;
-    char a = ' ';
-    cout << "Загадайте число від 1 до 100.\n\n";
-    while ((n - m) > 3 && count < 5)
-    {
-        cout << "Чи Ваше число менше, ніж " << x << "?\n";
-        cin >> a;
-        if (a == 'y') n = x;
-        else if (a == 'n') m = x;
-        else continue;
-        d = (m + n) / 2.0;
-        if ((m + n) % 2 == 1) {
-            if (!flag) { x = (int)d; flag = true; }
-            else { x = (int)d + 1; flag = false; }
-        }
-        else x = (int)d;
-        count++;
-    }
-    for (a = 'n'; a == 'n'; i++)
-    {
-        if (i == 3) break;
-        cout << "Ваше число " << m + i << "?\n";
-        cin >> a;
-        if (a == 'y') x = m + i;
-    }
-    if (a == 'n') x = m;
-    cout << "Чудово! Значить, Ви задумали число " << x << ".\n";
+   double a = 0, b = 0, res = 0;
+  char oper;
+  string act;
+  cin >> a;
+  cin >> oper;
+  cin >> b;
+  switch (oper)
+  {
+  case '+':
+    res = a + b;
+    act = "Sum";
+    break;
+  case '-':
+    res = a - b;
+    act = "Difference";
+    break;
+  case '*':
+    res = a * b;
+    act = "Multiplication";
+    break;
+  case '/':
+    res = a / b;
+    act = "Division";
+    break;
+  default:
+    cout << "Incorrect operation!\n";
+    break;
+  }
+  cout << act << " of " << a << " and " << b << " is " << res << endl;
 }
