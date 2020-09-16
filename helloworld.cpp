58bf4182557965503d65b26e56a3dccfd6b93680
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-09-16 22:23:22 +0300

    ex. 4.9 (grains of rice)

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,51 +2,18 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
-double num () {
-  string s;
-  vector<string> str = { "one","two","three","four","five","six","seven","eight","nine" };
-  bool flag = false;
-  double d = 0;
-  cin >> s;
-  for (int i = 0; i < str.size(); i++) {
-    if (s==str[i]) {d = i+1; flag = true; break;}
-  }
-  if (!flag) d = stof(s);
-  return d;
-}
-
 int main()
 {
-  double a = 0, b = 0, res = 0;
-  char oper;
-  string act;
-  a = num();
-  cin >> oper;
-  b = num();
-  switch (oper)
-  {
-  case '+':
-    res = a + b;
-    act = "Sum";
-    break;
-  case '-':
-    res = a - b;
-    act = "Difference";
-    break;
-  case '*':
-    res = a * b;
-    act = "Multiplication";
-    break;
-  case '/':
-    res = a / b;
-    act = "Division";
-    break;
-  default:
-    cout << "Incorrect operation!\n";
-    break;
+  double number = 1, cells = 1;
+  cout << setprecision(16);
+  cout << cells << " cell, " << number << " grain of rice \n";
+  while(cells<64) {
+    number*=2;
+    cells++;
+    cout << cells << " cells, " << number << " grains of rice \n";
   }
-  cout << act << " of " << a << " and " << b << " is " << res << endl;
 }
