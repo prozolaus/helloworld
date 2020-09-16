92156bbcd1691496fcc53f3716366ffa7e39b9da
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-09-16 19:42:26 +0300

    ex. 4.7 (calculator with words)

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,16 +5,48 @@
 
 using namespace std;
 
+double num () {
+  string s;
+  vector<string> str = { "one","two","three","four","five","six","seven","eight","nine" };
+  bool flag = false;
+  double d = 0;
+  cin >> s;
+  for (int i = 0; i < str.size(); i++) {
+    if (s==str[i]) {d = i+1; flag = true; break;}
+  }
+  if (!flag) d = stof(s);
+  return d;
+}
+
 int main()
 {
-  vector<string> str = { "one","two","three","four","five","six","seven","eight","nine" };
-  int number;
-  string s;
-  cin >> number;
-  cout << str[number - 1] << endl;
-  cin >> s;
-  for (int i = 0; i < str.size(); i++)
+  double a = 0, b = 0, res = 0;
+  char oper;
+  string act;
+  a = num();
+  cin >> oper;
+  b = num();
+  switch (oper)
   {
-    if (s == str[i]) cout << i+1 << endl;
+  case '+':
+    res = a + b;
+    act = "Sum";
+    break;
+  case '-':
+    res = a - b;
+    act = "Difference";
+    break;
+  case '*':
+    res = a * b;
+    act = "Multiplication";
+    break;
+  case '/':
+    res = a / b;
+    act = "Division";
+    break;
+  default:
+    cout << "Incorrect operation!\n";
+    break;
   }
+  cout << act << " of " << a << " and " << b << " is " << res << endl;
 }
