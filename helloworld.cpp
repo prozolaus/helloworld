058d7f058f544150718d7e0f367197ca773fb1c5
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-09-27 14:53:32 +0300

    ex. 4.15 (n prime numbers)

--- helloworld.cpp
+++ helloworld.cpp
@@ -15,10 +15,11 @@ void print_vector(vector<int> a) {
 
 int main()
 {
-  int n = 100;
+  int n, p;
   vector<int> numbers, primes;
-  cout << "Input max number: ";
-  cin >> n;
+  cout << "Input number of prime numbers: ";
+  cin >> p;
+  n = p*10;
   cout << endl;
   for (int i = 0; i < n; i++)
     numbers.push_back(i);
@@ -27,7 +28,11 @@ int main()
     if (numbers[i]!=0)
       for (int j = i*2; j < n; j+=i)
         numbers[j] = 0;
-  for (int i = 0; i < n; i++)
-    if (numbers[i]!=0) primes.push_back(i);
+  for (int i = 0, j = 0; i < n && j < p; i++)
+    if (numbers[i]!=0) {
+      primes.push_back(i);
+      j++;
+    }
+  cout << p << " prime numbers from 2 to " << primes[p-1] << ": \n";
   print_vector(primes);
 }
