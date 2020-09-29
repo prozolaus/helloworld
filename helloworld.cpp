fa5e9d01f2e030e908af431adabbeb23e69101db
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-09-29 21:24:59 +0300

    ex. 4.16 (Moda of numbers without set)

--- helloworld.cpp
+++ helloworld.cpp
@@ -11,15 +11,14 @@ using namespace std;
 void print_vector(vector<int> a) {
   for (int i = 0; i < a.size(); i++) 
     cout << a[i] << " ";
-  cout << endl;
+  cout << endl << endl;
 }
 
 int main()
 {
   srand(static_cast<unsigned int>(time(0)));
-  set<int> st;
-  vector<int> numbers, counts;
-  const int n = 35;
+  vector<int> numbers, counts, maxs;
+  const int n = 10;
   int count, max = 0;
   for (int i = 0; i < n; i++) numbers.push_back(rand()%10);
   print_vector(numbers);
@@ -35,11 +34,15 @@ int main()
   }
   //print_vector(counts);
   for (int i = 0; i < n; i++)
-    if (max==counts[i]) st.insert(numbers[i]);
-  set<int> :: iterator it = st.begin();
+    if (max==counts[i]) {
+      bool flag = false;
+      for (int j = 0; j < maxs.size(); j++)
+      {
+        if (numbers[i]==maxs[j]) flag = true;
+      }
+      if (!flag) maxs.push_back(numbers[i]);
+    }
   cout << "Moda: " ;
-  for (; it != st.end(); it++) {
-    cout << *it << " ";
-  }
-  cout << endl;
+  print_vector(maxs);
+  //cout << endl;
 }
