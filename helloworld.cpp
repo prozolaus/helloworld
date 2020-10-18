aa1c57769c5e1a8edd02d208a0391a7e43c00edd
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-10-18 14:46:56 +0300

    ex. 5.10 (errors + isDouble)

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,32 +2,47 @@
 
 using namespace std;
 
-bool IsNumber(const string &s) {
-    return all_of(s.cbegin(),s.cend(),[](const char &c) {
-        return isdigit(c);
-    });
+bool is_double(const std::string& s)
+{
+    std::istringstream iss(s);
+    double d;
+    return iss >> d >> std::ws && iss.eof();
 }
+
+class isnt_double {};
 
 int main()
 try {
-    int n, sum = 0;
+    int n;
+    double sum = 0;
     string s;
-    vector<int> v;
+    vector<double> v, dif;
     cout << "Enter the number of values to add: ";
     cin >> n;
-    cout << "Enter multiple integers (| to end input): ";
+    cout << "Enter multiple real numbers (| to end input): ";
     for (;;)
     {
         cin >> s;
         if (s=="|") break;
-        if (!IsNumber(s)) error("It is not a number!");
-        v.push_back(stoi(s));
+        if (!is_double(s)) throw isnt_double();
+        v.push_back(stod(s));
     }
-    //if (n > v.size()) error("n is bigger than the size of the vector");
     for (int i = 0; i < n; i++)
         sum+=v[i];
     cout << "The sum of the first " << n << " numbers is " << sum << ".\n";
+    for (int i = 0; i < n-1; i++)
+        dif.push_back(v[i+1]-v[i]);
+    if (n>1) cout << "The difference between elements of the vector: ";
+    for (double x : dif)
+        cout << x << " ";
+    cout << endl;
 } catch (out_of_range) {
     cerr << "Error of the range!\n";
     return 1;
+} catch (isnt_double) {
+    cerr << "Input was wrong!\n";
+    return 2;
+} catch (...) {
+    cerr << "Something went wrong!\n";
+    return 3;
 }
