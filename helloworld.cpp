a3545310a4ca613109c13dd7a065b07c3af79bf1
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-10-24 22:24:32 +0300

    ex. 5.11 (Fibonacci)

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,47 +2,20 @@
 
 using namespace std;
 
-bool is_double(const std::string& s)
+int main() 
 {
-    std::istringstream iss(s);
-    double d;
-    return iss >> d >> std::ws && iss.eof();
-}
-
-class isnt_double {};
-
-int main()
-try {
-    int n;
-    double sum = 0;
-    string s;
-    vector<double> v, dif;
-    cout << "Enter the number of values to add: ";
-    cin >> n;
-    cout << "Enter multiple real numbers (| to end input): ";
-    for (;;)
+    int i = 0, c = 0;
+    const int max_i = 44;
+    double a = 1, b = 1;
+    cout << a << " " << b << " ";
+    while (i < max_i)
     {
-        cin >> s;
-        if (s=="|") break;
-        if (!is_double(s)) throw isnt_double();
-        v.push_back(stod(s));
+        c = narrow_cast<int>(b + a);
+        cout << c << " ";
+        a = b;
+        b = c;
+        i++;
     }
-    for (int i = 0; i < n; i++)
-        sum+=v[i];
-    cout << "The sum of the first " << n << " numbers is " << sum << ".\n";
-    for (int i = 0; i < n-1; i++)
-        dif.push_back(v[i+1]-v[i]);
-    if (n>1) cout << "The difference between elements of the vector: ";
-    for (double x : dif)
-        cout << x << " ";
-    cout << endl;
-} catch (out_of_range) {
-    cerr << "Error of the range!\n";
-    return 1;
-} catch (isnt_double) {
-    cerr << "Input was wrong!\n";
-    return 2;
-} catch (...) {
-    cerr << "Something went wrong!\n";
-    return 3;
+    cout << "\nMax int that may contain this sequence is " << c << endl;
+    
 }
