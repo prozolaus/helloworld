87cc4fe2e51fd3fad15f39586660ee2301216599
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-11-14 15:00:59 +0200

    ex. 6.6 with cin.ignore(cin.rdbuf()->in_avail());

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,54 +1,99 @@
 #include "std_lib_facilities.h"
 
-class Name_value
-{
-public:
-    string names;
-    double scores;
-    Name_value(string n, double s)
-        : names(n), scores(s) {}
-};
+vector<string> articles = {"a", "an", "the", "A", "An", "The"};
+vector<string> nouns = {"C++", "birds", "bird"};
+vector<string> verbs = {"fly", "flies", "rule", "rules"};
+vector<string> unions = {"but", "and", "or"};
 
-Name_value get()
+bool articles_check()
 {
-    string str1;
-    char ch;
-    cin >> str1 >> ch;
-    switch (ch)
-    {
-    case '.':
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-        cin.putback(ch);
-        double d;
-        cin >> d;
-        return Name_value{str1, d};
-    default:
-        error("Wrong score!");
-    }
+    string s;
+    cin >> s;
+    for (int i = 0; i < articles.size(); i++)
+        if (s == articles[i])
+            return true;
+    return false;
+}
+
+bool nouns_check()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < nouns.size(); i++)
+        if (s == nouns[i])
+            return true;
+    return false;
+}
+
+bool verbs_check()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < verbs.size(); i++)
+        if (s == verbs[i])
+            return true;
+    return false;
+}
+
+bool unions_check()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < unions.size(); i++)
+        if (s == unions[i])
+            return true;
+    return false;
+}
+
+bool dot_check()
+{
+    if (cin.get() == '\n')
+        return false;
+    char s;
+    cin >> s;
+    if (s == '.')
+        return true;
+    return false;
 }
 
 int main()
 {
-    vector<Name_value> n;
-    while (true)
+    bool a = true, b = true, c = true, u = false, dot = false;
+    while (cin)
     {
-        Name_value t = get();
-        if (t.names == "NoName" && t.scores == 0)
-            break;
-        for (int i = 0; i < n.size(); i++)
-            if (t.names == n[i].names)
-                error("Error! You have already inputted this name!");
-        n.push_back(t);
+        for (int i = 0; i < 2; i++)
+        {
+            a = articles_check();
+            if (a)
+            {
+                b = nouns_check();
+                if (b)
+                {
+                    c = verbs_check();
+                    if (c)
+                    {
+                        if (i == 0)
+                        {
+                            u = unions_check();
+                            if (!u)
+                                break;
+                        }
+                        else
+                            dot = dot_check();
+                    }
+                    else
+                        break;
+                }
+                else
+                    break;
+            }
+            else
+                break;
+        }
+        if (!a || !b || !c || !u || !dot)
+            cout << "No!" << endl;
+        else
+            cout << "Yes!" << endl;
+        cin.ignore(cin.rdbuf()->in_avail());
     }
-    for (int i = 0; i < n.size(); i++)
-        cout << n[i].names << " has " << n[i].scores << " scores." << endl;
 }
