5e1d892ccb18222dfe7b51f91f53762086e77286
Nikolai Prozorov <prozorovmykola@gmail.com>
2020-11-22 23:38:58 +0200

    ex. 6.10 (permutations and combinations)

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,61 +2,29 @@
 
 using namespace std;
 
-void check(string s)
+double fact(double x)
 {
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] < '0' || s[i] > '9')
-            error("It is not a number!");
-    }
-    if (s.size() > 4)
-        error("The number of digits is more than 4.");
+    if (!x) return 1;
+    double sum = x;
+    for (double i = x-1; i > 1; i--)
+        sum *= i;
+    return sum;
 }
 
-void units(string s)
+int main() 
 {
-    cout << s[0] << " - units.";
-}
-
-void tens(string s)
-{
-    cout << s[0] << " - tens, ";
-    string temp_s(s.begin()+1, s.end());
-    units(temp_s);
-}
-
-void hundreds(string s)
-{
-    cout << s[0] << " - hundreds, ";
-    string temp_s(s.begin()+1, s.end());
-    tens(temp_s);
-}
-
-void thousands(string s)
-{
-    cout << s[0] << " - thousands, ";
-    string temp_s(s.begin()+1, s.end());
-    hundreds(temp_s);
-}
-
-int main()
-{
-    int num;
-    while (cin)
-    {
-        string s, s2;
-        cin >> s;
-        check(s);
-        num = stoi(s);
-        s2 = to_string(num);
-        if (s2.size() == 4)
-            thousands(s2);
-        else if (s2.size() == 3)
-            hundreds(s2);
-        else if (s2.size() == 2)
-            tens(s2);
-        else
-            units(s2);
-        cout << endl;
-    }
+    double a, b, p, c;
+    int i, j;
+    cout << "Input two integers separated by a space: ";
+    cin >> a >> b;
+    j = (int)b;
+    i = (int)a;
+    if (a-i || b-j) error("The first and second numbers must be integers!");
+    if (a<1 || b<1) error("The integers must be more than zero!");
+    if (a>100) error("The range must be from 1 to 100!");
+    if (a<b) error("The second integer mustn't be more than first!");
+    p = fact(a) / fact(a-b);
+    c = p / fact(b);
+    cout << "P(" << a << "," << b << ") = " << p << endl;
+    cout << "C(" << a << "," << b << ") = " << c << endl;
 }
